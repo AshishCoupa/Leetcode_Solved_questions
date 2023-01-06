545b7348709800633bf2feeee1493610c6faefc2
Ashish Kumar Patel <105703743+Ashishh0007@users.noreply.github.com>
2023-01-07 00:51:28 +0530

    Added solution - LeetHub

--- Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
+++ Partition Equal Subset Sum - GFG/partition-equal-subset-sum.cpp
@@ -29,6 +29,29 @@ class Solution{
         return dp[index][target] = incl or excl;
     }
     
+    bool solveTab(int arr[], int N, int total) {
+        vector<vector<int> > dp(N+1, vector<int> (total+1, 0));
+        
+        for(int i=0; i<=N; i++) {
+            dp[i][0] = 1;
+        }
+        
+        for(int index = N-1; index >= 0; index--) {
+            for(int target = 0; target <= total/2; target++) {
+                
+                int incl = 0;
+                if(target - arr[index] >= 0)
+                    incl = dp[index+1][target - arr[index]];
+                
+                int excl = dp[index+1][target];
+                
+                dp[index][target] = incl or excl;
+            }
+        }
+        
+        return dp[0][total/2];
+    }
+    
 public:
     int equalPartition(int N, int arr[])
     {
@@ -42,8 +65,8 @@ public:
         
         int target = total/2;
         
-        vector<vector<int> > dp(N, vector<int> (target+1, -1));
-        return solveMem(0, arr, N, target, dp);
+        
+        return solveTab(arr, N, total);
     }
 };
 
